fb909e548da0fb075c706b4e02b79c112db80322
Kutmanbekov Iskender <kutmanbekov_i@auca.kg>
2020-09-10 20:54:29 +0600

    accepted

---
+++ Kattis/faktor/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int sum_of_digits(int n);
+
+int main()
+{
+    int a, i;
+
+    cin >> a >> i;
+
+    int result = a * (i - 1) + 1;
+
+    cout << result << "\n";
+}
