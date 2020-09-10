ece97de34f82a14077927e675b5109a2e571c10a
Kutmanbekov Iskender <kutmanbekov_i@auca.kg>
2020-09-10 22:51:25 +0600

    wrong answer test 1/10

---
+++ Kattis/thelastproblem/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    getline(cin, s);
+
+    //Thank you, Twilight, and farewell!
+    cout << "Thank you, " << s << ", and farewell!!";
+}
