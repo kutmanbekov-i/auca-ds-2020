802dd05f53a9b40dc3ad5ff9b976cb08ba8958d5
Kutmanbekov Iskender <kutmanbekov_i@auca.kg>
2020-09-18 17:51:34 +0600

    acceptEd

--- Kattis/hothike/main.cpp
+++ Kattis/hothike/main.cpp
@@ -4,25 +4,22 @@ using namespace std;
 
 int main() {
     
-    int n, ind = 0, max_t = 0, hike_t = 0;
+    int n, ind = 0, max_t = 41, hike_t = 0;
     cin >> n;
     
     int a[n];
     
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
         cin >> a[i];
-        if (i < n - 2 and a[i] >= max_t)
-        {
-            max_t = a[i];
+        
+    for (int i = 0; i < n - 2; i++)
+    {
+        int temp = max(a[i], a[i + 2]);
+        if (temp < max_t) {
+            max_t = temp;
             ind = i;
         }
     }
-    if (ind == 0)
-        hike_t = max(a[ind], a[ind + 2] );
-    else if (ind == n - 1)
-        hike_t = max(a[ind], a[ind - 2]);
-    else
-        hike_t = max(a[ind - 1], a[ind + 1]);
-    cout << (ind == 0 ? 1 : ind) << " " <<  hike_t; 
+    cout << ind + 1 << " " <<  max_t; 
     
 }
