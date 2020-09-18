2ca3414ce6b79265ffc6228dd74e742f4ad372b3
Kutmanbekov Iskender <kutmanbekov_i@auca.kg>
2020-09-18 17:47:33 +0600

    aCcePTed

--- Kattis/mia/main.cpp
+++ Kattis/mia/main.cpp
@@ -15,24 +15,24 @@ int main() {
         
         if (first == second) cout << "Tie.\n";
         
-        else if (first[0] == '2' and first[1] == '1')
-            printf("Player 1 wins.\n");
-        else if (second[0] == '2' and second[1] == '1')
-            printf("Player 2 wins.\n");
-        
-        else if (first[0] == first[1] and second[0] == second[1])
-        {
-            printf("Player %d wins.\n", first[0] > second[0] ? 1 : 2);
-        }
-        else if (first[0] == first[1]) {
-            printf("Player 1 wins.\n");
-        }
-        else if (second[0] == second[1]) {
-            printf("Player 2 wins.\n");
-        }
         else {
-            printf("Player %d wins.\n", first[0] > second[0] ? 1 : 2);
+            int x = stoi(first);
+            int y = stoi(second);
+            int winner = x > y ? 1 : 2;
+            
+            if (x == 21 or y == 21)
+                winner = ( x == 21 ? 1 : 2 );
+            
+            else if (first[0] == first[1] and second[0] == second[1])
+                winner = x > y ? 1 : 2;
+                
+            else if (first[0] == first[1])
+                winner = 1;
+            
+            else if (second[0] == second[1])
+                winner = 2;
+                
+            cout << "Player " << winner << " wins.\n";
         }
-        
     }
 }
