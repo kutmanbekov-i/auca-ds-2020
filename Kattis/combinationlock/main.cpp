338fb1a15eaecd0e68fe5567a48c831bc51f1740
Kutmanbekov Iskender <kutmanbekov_i@auca.kg>
2020-09-18 17:39:04 +0600

    still wroNG ANSWer test 2/2

--- Kattis/combinationlock/main.cpp
+++ Kattis/combinationlock/main.cpp
@@ -11,17 +11,18 @@ int main() {
         if (x == 0 and a == 0 and b == 0 and c == 0) break;
         
         // 360 / 40 = 9
+        
         // dial: 0, 1, 2 ... , 39. (40 values)
         
         deg += 720;
         
-        deg += ( (40 - abs(a - x)) * 9 );
+        deg += ( (40 - abs(a - x) % 40) * 9 );
         
         deg += 360;
         
-        deg += ( (40 - abs(b - a)) * 9 );
+        deg += ( (40 - abs(b - a) % 40) * 9 );
         
-        deg += ( (40 - abs(c - b)) * 9 );
+        deg += ( (40 - abs(c - b) % 40) * 9 );
         
         cout << deg << "\n";
         deg = 0;
