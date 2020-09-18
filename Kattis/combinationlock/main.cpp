fb8eacf449df0b08784eea9f9216c3a7d8d4b751
Kutmanbekov Iskender <kutmanbekov_i@auca.kg>
2020-09-18 17:38:00 +0600

    wrONG ANSwer test 2/2

---
+++ Kattis/combinationlock/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    
+    int x, a, b, c, deg = 0;
+    
+    while (cin >> x >> a >> b >> c)
+    {
+        if (x == 0 and a == 0 and b == 0 and c == 0) break;
+        
+        // 360 / 40 = 9
+        // dial: 0, 1, 2 ... , 39. (40 values)
+        
+        deg += 720;
+        
+        deg += ( (40 - abs(a - x)) * 9 );
+        
+        deg += 360;
+        
+        deg += ( (40 - abs(b - a)) * 9 );
+        
+        deg += ( (40 - abs(c - b)) * 9 );
+        
+        cout << deg << "\n";
+        deg = 0;
+    }
+}
