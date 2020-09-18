3809f3c26ac0bf3d7a9c5556e4373dfcd2d3eb32
Kutmanbekov Iskender <kutmanbekov_i@auca.kg>
2020-09-18 17:30:35 +0600

    wRoNg AnSwEr test 1

--- Kattis/stararrangements/main.cpp
+++ Kattis/stararrangements/main.cpp
@@ -16,13 +16,14 @@ int main() {
         if ( s % ((x + y) * n) == 0 or s % (((x + y) * n) + x) == 0)
         {
             cout << x << "," << y << "\n";
-            
         }
         
-        if (x > s / 2) break;
+        if (x >= s / 2) break;
         
         if (x > y) y++;
         else x++;
 
     }
+    if (s % 2 == 1) cout << s / 2 + 1 << "," << s / 2 << "\n";
+    else            cout << s / 2 + 0 << "," << s / 2 << "\n";
 }
