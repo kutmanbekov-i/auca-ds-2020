bad3143af3a25ba03b30b58a2d4644caac079195
Kutmanbekov Iskender <kutmanbekov_i@auca.kg>
2020-09-18 17:35:39 +0600

    acCEptED

--- Kattis/thanos/main.cpp
+++ Kattis/thanos/main.cpp
@@ -1,22 +1,23 @@
 #include <bits/stdc++.h>
 
 typedef unsigned long long ull;
-typedef long double ld;
 
 using namespace std;
 
 int main() {
     
-    ull t, p, r, f;
-    ld res;
-    
+    ull t, p, r, f, cnt = 0;
     cin >> t;
     
     while(t--)
     {
         cin >> p >> r >> f;
+        cnt = 0;
         
-        res = (ld)f / (p * r);
-        cout << (res < 1 ? f >= p ? 1 : 0 : ceil(res)) << "\n";
+        while(p <= f) {
+            p *= r;
+            cnt++;
+        }
+        cout << cnt << "\n";
     }
 }
