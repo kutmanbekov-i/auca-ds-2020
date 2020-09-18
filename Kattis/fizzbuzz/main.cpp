7776b58d6c7da94ed6cd20236c1f3e9e1bcff12c
Kutmanbekov Iskender <kutmanbekov_i@auca.kg>
2020-09-18 17:24:08 +0600

    accepted

--- Kattis/fizzbuzz/main.cpp
+++ Kattis/fizzbuzz/main.cpp
@@ -9,7 +9,7 @@ int main() {
     
     for(int i = 1; i <= n; i++)
     {
-        if (i % x == 0 and i % y == 0) cout << "FizzBuss\n";
+        if (i % x == 0 and i % y == 0) cout << "FizzBuzz\n";
         else if (i % x == 0) cout << "Fizz\n";
         else if (i % y == 0) cout << "Buzz\n";
         else cout << i << "\n";
