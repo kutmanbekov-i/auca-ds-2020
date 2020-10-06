01f2fab783ab7756a574656fd9f985f5c822c56f
Kutmanbekov Iskender <kutmanbekov_i@auca.kg>
2020-10-07 01:37:12 +0600

    UVA: 10196 ACCCEPPTTEEDDDDD!!!!!! VERY STRANGE DEBUGGING

--- lab03/p14/main.cpp
+++ lab03/p14/main.cpp
@@ -2,17 +2,15 @@
 
 using namespace std;
 
-char board[8][8];
+char board[12][12];
 
-bool check_HV(int team, int i, int j);
-
-bool check_DIAGONALS(int team, int i, int j);
-
-bool check_kon(int team, int i, int j);
+bool check_king(pair<int, int> &king, int shift);
 
 int main() {
     
     bool empty = false;
+    
+    pair<int, int> white, black;
 
     int t = 1;
     
@@ -20,305 +18,170 @@ int main() {
     {
         empty = true;
         
-        for (int i = 0; i < 8; ++i)
-            for (int j = 0; j < 8; ++j)
+        for (int i = 0; i < 12; ++i)
+            for (int j = 0; j < 12; ++j)
+                board[i][j] = '.';
+        
+        for (int i = 2; i < 10; ++i)
+            for (int j = 2; j < 10; ++j)
             {
-                // scanf(" %c", &board[i][j]);
-                cin >> board[i][j];
-                if (board[i][j] != '.' or !board[i][j])
+                scanf(" %c", &board[i][j]);
+                // cin >> board[i][j];
+                if (board[i][j] != '.')
                     empty = false;
+                
+                if (board[i][j] == 'k')
+                {
+                    black.first = i;
+                    black.second = j;
+                }
+                else if (board[i][j] == 'K')
+                {
+                    white.first = i;
+                    white.second = j;
+                }
             }
         
         if (empty)
             break;
-            
-        bool white = false;
-        bool black = false;
         
-        for (int i = 0; i < 8; ++i)
-        {
-            for (int j = 0; j < 8; ++j)
-            {
-                // checking kings for lose
-                if (board[i][j] == 'k')
-                    black = check_HV(2, i, j) or check_DIAGONALS(2, i, j) or check_kon(2, i, j);
-                else if (board[i][j] == 'K')
-                    white = check_HV(1, i, j) or check_DIAGONALS(1, i, j) or check_kon(1, i, j);
-                if (white or black)
-                    break;
-            }
-            if (white or black) break;
-        }
+        bool white_shah = check_king(white, 32);
+        bool black_shah = check_king(black, 0);
+        
 
-        if (black and !white)
+        if (black_shah and !white_shah)
             printf("Game #%d: black king is in check.\n", t++);
-        else if (!black and white)
+        else if (white_shah and !black_shah)
             printf("Game #%d: white king is in check.\n", t++);
         else 
             printf("Game #%d: no king is in check.\n", t++);
 
-        cin.ignore();
+        // cin.ignore();  
     }
 }
 
-bool check_HV(int team, int i, int j)
+
+bool check_king(pair<int, int> &king, int shift)
 {
-    char p, r, b, q, my_k, n;
-
-    // black lost?
-    if (team == 2)
-    {
-        p = 'P';
-        r = 'R';
-        b = 'B';
-        q = 'Q';
-        my_k = 'k';
-        n = 'N';
-    } else {
-        p = 'p';
-        r = 'r';
-        b = 'b';
-        q = 'q';
-        my_k = 'K';
-        n = 'n';
-    }
+    //enemy figures
+    char pawn = char('P' + shift);
+    char rook = char('R' + shift);
+    char bishop = char('B' + shift);
+    char queen = char('Q' + shift);
+    char knight = char('N' + shift);
+    char enemy_king = char('K' + shift);
     
-    // check verticals
-    bool king_met = false;
-    bool enemy_met = false;
-    for (int k = 0; k < 8; ++k)
+    //up
+    for (int i = king.first - 1, j = king.second; i >= 2; --i)
     {
-        if (board[k][j] == my_k) 
+        if (board[i][j] != '.')
         {
-            if (enemy_met)
+            if(board[i][j] == rook or board[i][j] == queen)
                 return true;
-            king_met = true;
-        }
-        
-        else if (board[k][j] != '.')
-        {
-            if (board[k][j] == r or board[k][j] == q)
-            {
-                if (king_met)
-                    return true;
-                enemy_met = true;
-            }
-            else
-            {
-                if (king_met or enemy_met)
-                    break;
-            }
-        }
-    }
-    
-    king_met = false;
-    enemy_met = false;
-    
-    //check horizontals
-    for (int k = 0; k < 8; ++k)
-    {
-        if (board[i][k] == my_k) 
-        {
-            if (enemy_met)
-                return true;
-            king_met = true;
-        }
-        
-        else if (board[i][k] != '.')
-        {
-            if (board[i][k] == r or board[i][k] == q)
-            {
-                if (king_met)
-                    return true;
-                enemy_met = true;
-            }
-            else
-            {
-                if (king_met or enemy_met)
-                    break;
-            }
-        }
-    }
-    
-    return false;
-}
-
-bool check_DIAGONALS(int team, int i, int j)
-{
-    bool king_met = false;
-    bool enemy_met = false;
-
-    char p, r, b, q, my_k, n;
-
-    // black lost?
-    if (team == 2)
-    {
-        p = 'P';
-        r = 'R';
-        b = 'B';
-        q = 'Q';
-        my_k = 'k';
-        n = 'N';
-        
-        //checking for pawn
-        if (i < 7 and j < 7 and j > 0 and board[i + 1][j + 1] == p or board[i + 1][j - 1] == p)
-            return true;
-        
-    } else {
-        p = 'p';
-        r = 'r';
-        b = 'b';
-        q = 'q';
-        my_k = 'K';
-        n = 'n';
-        
-        //checking for pawn
-        if ( i > 0 and j > 0 and j < 7 and board[i - 1][j + 1] == p or board[i - 1][j - 1] == p)
-            return true;
-    }
-
-    int t = min(i, j);
-    int ki = i - t;
-    int kj = j - t;
-
-    // down right
-    for (; max(ki, kj) < 8; ++ki, ++kj)
-    {
-        if (board[ki][kj] == my_k) 
-        {
-            if (enemy_met)
-                return true;
-            king_met = true;
-        }
-        
-        else if (board[ki][kj] != '.')
-        {
-            if (board[ki][kj] == b or board[ki][kj] == q)
-            {
-                if (king_met)
-                    return true;
-                enemy_met = true;
-            }
-            else
-            {
-                if (king_met or enemy_met)
-                    break;
-            }
-        }
-    }
-
-    king_met = false;
-    enemy_met = false;
-
-    t = max(i, j);
-
-    if (i + j <= 7) { // above the secondary
-        ki = 0;
-        kj = i + j - ki;
-    } else {        // below
-        kj = 7;     //the bug must be here!
-        ki = i + j - kj; 
-    }
-
-    bool I = (ki != 0);
-
-    // down left
-    for (; ;)
-    {
-        if (board[ki][kj] == my_k) 
-        {
-            if (enemy_met)
-                return true;
-            king_met = true;
-        }
-        
-        else if (board[ki][kj] != '.')
-        {
-            if (board[ki][kj] == b or board[ki][kj] == q)
-            {
-                if (king_met)
-                    return true;
-                enemy_met = true;
-            }
-            else
-            {
-                if (king_met or enemy_met)
-                    break;
-            }
-        }
-        if (I)
-        {
-            --ki;
-            ++kj;
-            if (ki == 0) break;
-        } else {
-            --kj;
-            ++ki;
-            if (kj == 0) break;
-        }
-    }
-
-    return false;
-}
-
-
-bool check_kon(int team, int y, int x)
-{
-    char n = team == 2 ? 'N' : 'n';
-    
-    if (x - 2 >= 0)
-    {
-        if (y - 1 >= 0 && board[y - 1][x - 2] == n)
-            return true;
-        
-        if (y + 1 < 8 && board[y + 1][x - 2] == n)
-            return true;
-    }
-    
-    if (x + 2 < 8)
-    {
-        if (y - 1 >= 0 && board[y - 1][x + 2] == n)
-            return true;
             
-        if (y + 1 < 8 && board[y + 1][x + 2] == n)
-            return true;
+            else break;
+        }
     }
     
-    if (y - 2 >= 0)
+    //down
+    for (int i = king.first + 1, j = king.second; i < 10; ++i)
     {
-        if (x - 1 >= 0 && board[y - 2][x - 1] == n)
-            return true;
-        
-        if (x + 1 < 8 && board[y - 2][x + 1] == n)
-            return true;
-    }
-    
-    if (y + 2 < 8)
-    {
-        if (x - 1 >= 0 && board[y + 2][x - 1] == n)
-            return true;
+        if (board[i][j] != '.')
+        {
+            if(board[i][j] == rook or board[i][j] == queen)
+                return true;
             
-        if (x + 1 < 8 && board[y + 2][x + 1] == n)
-            return true;
+            else break;
+        }
     }
     
+    //left
+    for (int i = king.first, j = king.second - 1; j >= 2; --j)
+    {
+        if (board[i][j] != '.')
+        {
+            if(board[i][j] == rook or board[i][j] == queen)
+                return true;
+            
+            else break;
+        }
+    }
+    
+    //right
+    for (int i = king.first, j = king.second + 1; j < 10; ++j)
+    {
+        if (board[i][j] != '.')
+        {
+            if(board[i][j] == rook or board[i][j] == queen)
+                return true;
+            
+            else break;
+        }
+    }
+    
+    //up-left
+    for (int i = king.first - 1, j = king.second - 1; i >= 2 and j >= 2; --i, --j)
+    {
+        if (board[i][j] != '.')
+        {
+            if (board[i][j] == bishop or board[i][j] == queen)
+                return true;
+                
+            else break;
+        }
+    }
+    
+    //up-right
+    for (int i = king.first - 1, j = king.second + 1; i >= 2 and j < 10; --i, ++j)
+    {
+        if (board[i][j] != '.')
+        {
+            if (board[i][j] == bishop or board[i][j] == queen)
+                return true;
+                
+            else break;
+        }
+    }
+    
+    //down-left
+    for (int i = king.first + 1, j = king.second - 1; i < 10 and j >= 2; ++i, --j)
+    {
+        if (board[i][j] != '.')
+        {
+            if (board[i][j] == bishop or board[i][j] == queen)
+                return true;
+                
+            else break;
+        }
+    }
+    
+    //down-right
+    for (int i = king.first + 1, j = king.second + 1; i < 10 and j < 10; ++i, ++j)
+    {
+        if (board[i][j] != '.')
+        {
+            if (board[i][j] == bishop or board[i][j] == queen)
+                return true;
+                
+            else break;
+        }
+    }
+    
+    int i = king.first, j = king.second;
+    
+    //pawns
+    if (shift == 32)
+        if (board[i - 1][j - 1] == pawn or board[i - 1][j + 1] == pawn)
+            return true;
+    if (shift == 0)
+        if (board[i + 1][j + 1] == pawn or board[i + 1][j - 1] == pawn)
+            return true;
+
+    //knights
+    if (board[i - 2][j - 1] == knight or board[i - 1][j - 2] == knight or board[i + 1][j - 2] == knight or board[i + 2][j - 1] == knight or
+        board[i + 2][j + 1] == knight or board[i + 1][j + 2] == knight or board[i - 1][j + 2] == knight or board[i - 2][j + 1] == knight)
+        return true;
+        
     return false;
-    
-    // char my_k, n;
-    
-    // if (team == 2)
-    // {
-    //     my_k = 'k';
-    //     n = 'N';
-    // } else {
-    //     my_k == 'K';
-    //     n = 'n';
-    // }
-    
-    // if (board[i - 1][j - 2] == n or board[i - 2][j - 1] == n or board[i - 2][j + 1] == n or board[i - 1][j + 2] == n or
-    //     board[i + 1][j - 2] == n or board[i + 2][j - 1] == n or board[i + 2][j + 1] == n or board[i + 1][j + 2] == n)
-    // {
-    //     return true;
-    // }
-    // return false;
 }
+
