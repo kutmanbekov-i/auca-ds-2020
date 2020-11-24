c14541b801a1a48895e95a1e603a9db34d15daae
Kutmanbekov Iskender <kutmanbekov_i@auca.kg>
2020-11-24 23:09:52 +0600

    UVA: 11321 – Wrong Answer!

---
+++ project01/uva-11321/main.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool compare(const int a, const int b);
+
+int m = 1;
+
+int main() {
+    
+    int n;
+    
+    while(1)
+    {
+        cin >> n >> m;
+        
+        cout << n << " " << m << '\n';
+        
+        if (n == 0 and m == 0) break;
+
+        vector<int> data(n);
+        
+        for (int i = 0; i < n; ++i)
+            cin >> data[i];
+        
+        sort(data.begin(), data.end(), compare);
+        
+        for (int x : data)
+            cout << x << '\n';
+    }
+}
+
+bool compare(const int a, const int b)
+{
+    if (a % m == b % m)
+    {
+        if (a % 2 == 0 and b % 2 == 0)
+        {
+            return a < b;
+        }
+        if (a % 2 == 1 and b % 2 == 1)
+        {
+            return a > b;
+        }
+        return (a % 2 == 1 and b % 2 == 0);
+    }
+    return a % m < b % m;
+}
