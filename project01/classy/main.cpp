bab0916477c58a62ad8d1d478f76d82ad2a4185d
Kutmanbekov Iskender <kutmanbekov_i@auca.kg>
2020-12-04 03:16:41 +0600

    Kattis: classy – WA at the test 1

---
+++ project01/classy/main.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void parse(const string &rank, string &parsed)
+{
+    int len = rank.length();
+    
+    for (int i = 0; i < len; ++i)
+    {
+        if (rank[i] == 'u')
+        {
+            parsed += 'a';
+            i += 5;
+        }
+        if (rank[i] == 'm')
+        {
+            parsed += 'b';
+            i += 6;
+        }
+        if (rank[i] == 'l')
+        {
+            parsed += 'c';
+            i += 5;
+        }
+    }
+    
+    reverse(parsed.begin(), parsed.end());
+    len = parsed.length();
+    
+    for (int i = len - 1; i < 10; ++i)
+    {
+        parsed += 'b';
+    }
+}
+
+int main() {
+    
+    int t; cin >> t;
+    
+    while (t--)
+    {
+        
+        int n; cin >> n;
+        
+        vector<pair<string, string>> data(n);
+        
+        for (int i = 0; i < n; ++i)
+        {
+            string name, rank, kolbasa, parsed = "";
+            
+            cin >> name >> rank >> kolbasa;
+            
+            parse(rank, parsed);
+            
+            data[i] = {name, parsed};
+        }
+        
+        sort(data.begin(), data.end(), []( pair<string, string> a, pair<string, string> b )
+        {
+            if (a.second == b.second)
+            {
+                return a.first < b.first;
+            }
+            return a.second < b.second;
+        });
+        
+        for (int i = 0; i < n; ++i)
+        {
+            cout << data[i].first.substr(0, data[i].first.length() - 1) << '\n';
+        }
+        
+        if (t) cout << "==============================\n";
+    }
+}
