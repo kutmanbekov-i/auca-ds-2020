d7cb8ac2418783896f6311a2b635afffa11215fe
Kutmanbekov Iskender <kutmanbekov_i@auca.kg>
2020-12-29 01:41:54 +0600

    Kattis: booking – AccEpTeD

--- project01/booking/main.cpp
+++ project01/booking/main.cpp
@@ -2,133 +2,113 @@
 
 using namespace std;
 
-enum class Event
-{
-    Departure,
-    Arrival,
-};
-
 struct Date
 {
-    int y;
-    int m;
-    int d;
-    
+    int y, m, d, h, mi;
+    int type;
 };
 
-const vector<int> monthDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+vector<int> days_in_month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-int compare(const string &departure, const string &arrival)
+int comparator (const void *a, const void *b)
 {
-    if (departure > arrival)
-    {
-        return -1;
-    }
+    Date d1 = *(Date*)a, d2 = *(Date*)b;
     
-    return 1;
+    if (d1.y != d2.y) return d1.y - d2.y;
+    if (d1.m != d2.m) return d1.m - d2.m;
+    if (d1.d != d2.d) return d1.d - d2.d;
+    if (d1.h != d2.h) return d1.h - d2.h;
+    if (d1.mi != d2.mi) return d1.mi - d2.mi;
+    return d1.type - d2.type;
 }
 
-int main() {
-    
+int rooms_needed(vector<Date> &dates, int size)
+{
+    int c = 0, most = 0;
+    for (int i = 0; i < size; i++) {
+        c += dates[i].type;
+        if (c > most) 
+            most = c;
+    }
+    return most;
+}
+
+void next_day(vector<Date> &dates, int i)
+{
+    // New year
+    if (dates[i].d == 31 && dates[i].m == 12) {
+        dates[i].y++;
+        dates[i].m = 1;
+        dates[i].d = 1;
+        return;
+    }
+
+    // Leap year exception
+    if (dates[i].y == 2016 && dates[i].m == 2 && dates[i].d >= 28)
+    {
+        if (dates[i].d == 28) {
+            dates[i].d = 29;
+        } else {
+            dates[i].d = 1;
+            dates[i].m++;
+        }
+        return;
+    }
+
+    // End of month
+    if (dates[i].d == days_in_month[dates[i].m - 1]) {
+        dates[i].d = 1;
+        dates[i].m++;
+        return;
+    }
+
+    // Any other day
+    dates[i].d++;
+}
+
+void add_cleaning_time(vector<Date> &dates, int i, int c)
+{
+    if (!c) return;
+
+    dates[i].mi += c;
+    dates[i].h += dates[i].mi / 60;
+    dates[i].mi %= 60;
+
+    if (dates[i].h >= 24) {
+        dates[i].h %= 24;
+        next_day(dates, i);
+    }
+}
+
+int main()
+{
     int t; cin >> t;
     
-    for (int i = 0; i < t; ++i)
+    while(t--)
     {
         int b, c;
         cin >> b >> c;
         
-        vector<pair<string, string>> data;
-        
-        for (int j = 0; j < b; ++j)
+        vector<Date> dates(b * 2);
+        b *= 2;
+        for (int i = 0; i < b; i += 2)
         {
-            string code, date1, time1, date2, time2;
-            
-            cin >> code >> date1 >> time1 >> date2 >> time2;
-            
-            data.emplace_back(date1 + " " + time1, date2 + " " + time2);
+            char code[21];
+            dates[i].type = 1;
+            dates[i + 1].type = -1;
+            scanf("%s %d-%d-%d %d:%d %d-%d-%d %d:%d", code, &dates[i].y,&dates[i].m,&dates[i].d,&dates[i].h,&dates[i].mi, &dates[i+1].y,&dates[i+1].m,&dates[i+1].d,&dates[i+1].h,&dates[i+1].mi);
+                
+            add_cleaning_time(dates, i + 1, c);
         }
-        
-        sort(data.begin(), data.end(), [](const pair<string, string> &a, const pair<string, string> &b)
-        {
-            return a.first < b.first;
-        });
-        
-        int cnt = b;
-        
-        for (int j = 0; j < b; ++j)
-        {
-            int year0 = stoi(data[j].second.substr(0, 4));
-            int month0 = stoi( data[j].second.substr(5, 7));
-            int day0 = stoi(data[j].second.substr(8, 10));
-            int hour0 = stoi(data[j].second.substr(11, 13));
-            int minute0 = stoi(data[j].second.substr(14));
-            
-            minute0 += c;
-            if (minute0 >= 60)
-            {
-                hour0 += minute0 / 60;
-                minute0 %= 60;
-            }
 
-            if (hour0 >= 24)
-            {
-                day0 += hour0 / 24;
-                hour0 %= 24;
-            }
-            
-            if (year0 == 2016)
-            {
-                if (month0 == 2 and day0 > monthDays[month0 - 1] + 1)
-                {
-                    // cout << "Hello, bug is hereeeeeeeeeeeeeee: " << month0 << " " << day0 << '\n';
-                    month0 += day0 / (monthDays[month0 - 1] + 1);
-                    day0 %= (monthDays[(month0 - 2) % 12] + 1);
-                }
-                else if (month0 != 2 and day0 > monthDays[month0 - 1])
-                {
-                    // cout << "Hello, bug is here: " << month0 << " " << day0 << '\n';
-                    month0 += day0 / monthDays[month0 - 1];
-                    day0 %= monthDays[(month0 - 2) % 12];
-                }
-            }
-            else
-            {
-               if (day0 > monthDays[month0 - 1])
-                {
-                    month0 += day0 / monthDays[month0 - 1];
-                    day0 %= monthDays[(month0 - 2) % 12];
-                    // cout << "H3llo, bug is here\n" << month0 << " " << day0 << '\n';
-                } 
-            }
-
-            if (month0 > 12)
-            {
-                year0 += month0 / 12;
-                month0 %= 12;
-            }
-            
-            string new_year = to_string(year0);
-            string new_month = (month0 < 10) ? "0" + to_string(month0) : to_string(month0);
-            string new_day = (day0 < 10) ? "0" + to_string(day0) : to_string(day0);
-            string new_hour = (hour0 < 10) ? "0" + to_string(hour0) : to_string(hour0);
-            string new_minute = (minute0 < 10) ? "0" + to_string(minute0) : to_string(minute0);
-            
-            data[j].second = new_year + "-" + new_month + "-" + new_day + " " + new_hour + ":" + new_minute;
-            
-            if (j)
-            {
-                int num = compare(data[j - 1].second, data[j].first);
-                if (num > 0)
-                {
-                    --cnt;
-                }
-            }
-        }
-        // cout << "========================================\n";
-        // for (const auto &p : data)
+        qsort(&dates[0], b, sizeof(Date), comparator);
+        
+        // for (int i = 0; i < b; i += 2)
         // {
-        //     cout << p.first << " " << p.second << endl;
+        //     cout << dates[i].y << "-" << dates[i].m << "-" << dates[i].d << " " << dates[i].h << ":" << dates[i].mi << ' ';
+        //     cout << dates[i + 1].y << "-" << dates[i + 1].m << "-" << dates[i + 1].d << " " << dates[i + 1].h << ":" << dates[i + 1].mi << '\n';
         // }
-        cout << cnt << endl;
+        printf("%d\n", rooms_needed(dates, b));
     }
+    return 0;
 }
